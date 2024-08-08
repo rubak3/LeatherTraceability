7dff82ce2751391f17d66bdb3c68128e33d334a5
rubak3 <ruba.sleem@gmail.com>
2024-08-08 19:29:32 +0400

    First Commit

--- Traceability.sol
+++ Traceability.sol
@@ -2,9 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "Registration.sol";
-import "HFACCertification.sol";
-import "LWGCertification.sol";
-import "OEKOTEXCertification.sol";
+import "Certifications.sol";
 
 contract Traceability {
 
